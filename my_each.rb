bfc7abdab70d6219f0cf264f69d4dfc254d08e9e
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-20 13:55:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  
+def my_each(array)
+  if block_given?
+    i = 0 
+    while i < array.length
+      yield 
+    end
+  end
 end
+
+my_each([1, 2, 3, 4]) do |num|
+  num 
+end 
